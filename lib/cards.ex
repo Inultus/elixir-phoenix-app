74029a8b44b94e9f03a18b251fdac3ff0a6717bf
Unknown <otheldobel@autistici.org>
2019-06-19 22:21:03 +0200

    deck method : create, shuffle, deal and contains

--- lib/cards.ex
+++ lib/cards.ex
@@ -6,8 +6,31 @@ defmodule Cards do
   @doc """
   """
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+#    cards = for suit <- suits do
+#              for value <- values do
+#                "#{value} of #{suit}"
+#              end
+#          end
+#    List.flatten(cards)
+
+    for suit <- suits, value <- values do
+            "#{value} of #{suit}"
+    end
   end
 
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
 
 end
